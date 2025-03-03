7d1af95f7b15c0aa227d24edd9768ad5e32a6a60
Simon Podlipsky <simon@podlipsky.net>
2025-03-03 08:54:58 +0100

    chore(deps): ratchet rfc6455 0.4

--- src/Ratchet/WebSocket/WsServer.php
+++ src/Ratchet/WebSocket/WsServer.php
@@ -1,5 +1,6 @@
 <?php
 namespace Ratchet\WebSocket;
+use Nyholm\Psr7\Factory\Psr17Factory;
 use Ratchet\ComponentInterface;
 use Ratchet\ConnectionInterface;
 use Ratchet\MessageComponentInterface as DataComponentInterface;
@@ -86,7 +87,7 @@ class WsServer implements HttpServerInterface {
         $this->connections = new \SplObjectStorage;
 
         $this->closeFrameChecker   = new CloseFrameChecker;
-        $this->handshakeNegotiator = new ServerNegotiator(new RequestVerifier);
+        $this->handshakeNegotiator = new ServerNegotiator(new RequestVerifier, new Psr17Factory());
         $this->handshakeNegotiator->setStrictSubProtocolCheck(true);
 
         if ($component instanceof WsServerInterface) {
